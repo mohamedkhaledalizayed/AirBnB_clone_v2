df25e13269a87738d1de2de0805f438e0326f90f
Mohamed Zayed <mohammedkhaled077@gmail.com>
2024-04-05 02:20:21 +0200

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,52 @@
 #!/usr/bin/python3
-"""This module contains methods"""
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ["54.160.72.119", "54.90.17.67"]
+"""
+Distributes an archive to my web servers,
+using the function do_deploy
+"""
+from fabric.api import *
+from datetime import datetime
+import os
+
+env.hosts = ['54.144.136.64', '100.24.236.179']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    '''
+    Generates a tgz archive from the
+    contents of the web_static folder
+    '''
+    try:
+        local('mkdir -p versions')
+        datetime_format = '%Y%m%d%H%M%S'
+        archive_path = 'versions/web_static_{}.tgz'.format(
+            datetime.now().strftime(datetime_format))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        print('web_static packed: {} -> {}'.format(archive_path,
+              os.path.getsize(archive_path)))
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """Moethod that distributes an archive to web servers"""
-    if exists(archive_path) is False:
+    '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
         return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
     try:
-        file_name = archive_path.split("/")[-1]
-        date_time = file_name.split(".")[0]
-        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, date_time))
-        run('tar -xzf /tmp/{} -C {}{}'.format(file_name, path, date_time))
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
         run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}'.format(path, date_time))
-        run('rm -rf {}{}/web_static/'.format(path, date_time))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, date_time))
-        print("New version deployed!")
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
         return True
-    except Exception:
+    except:
         return False
