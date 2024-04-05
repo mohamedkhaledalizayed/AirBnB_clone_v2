2dea9be09eee301d9d19445a67655a63a7c2bb5e
Mohamed Zayed <mohammedkhaled077@gmail.com>
2024-04-06 00:22:07 +0200

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-# Prepare web server for deployment
-apt-get -y update
-apt-get -y install nginx
-mkdir -p /data/web_static/shared/
+# Script that sets up web servers from the deployment of web_static.
+
+# install nginx if not exists
+apt update
+apt install nginx -y
+
+# create necessary directories and indes file
 mkdir -p /data/web_static/releases/test/
+if [ ! -d "/data/web_static/shared/" ]; then
+    mkdir /data/web_static/shared/
+fi
+touch /data/web_static/releases/test/index.html
 echo "<html>
   <head>
   </head>
@@ -11,7 +18,20 @@ echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# create a symbolik link
+link_path="/data/web_static/current"
+release_path="/data/web_static/releases/test/"
+if [ -L "$link_path" ]; then
+        rm "$link_path"
+fi
+ln -s "$release_path" "$link_path"
+
+# give recursive ownership for the data directory
 chown -R ubuntu:ubuntu /data/
-sed -i "61i\ \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}" /etc/nginx/sites-available/default
+
+# updating the nginx configuration
+sed -i '57i\\n\tlocation \/hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+# restart web server
 service nginx restart
